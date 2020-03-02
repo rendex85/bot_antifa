f397fe971509a9882dc728878f580a16adcd0ce0
rendex85 <rendex85@gmail.com>
2020-03-02 11:13:43 +0300

    получение юзеров, загрузка фотографий, класс для ответов с объектами

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import requests
 from vk_api import *
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
-from MembersConf import *
+from WorkWithUsers import *
 import login_consts
 from TextAnswer import  *
+from ObjAnswer import *
 
 from GetPicture import *
 
@@ -13,22 +14,30 @@ session = requests.Session()
 vk_session = vk_api.VkApi(token=login_consts.token)
 longpoll = VkBotLongPoll(vk_session,login_consts.public )
 vk = vk_session.get_api()
-upload = VkUpload(vk_session)
 
 
 
 for event in longpoll.listen():
     if event.type == VkBotEventType.MESSAGE_NEW:
         peer=event.obj.peer_id
-        answer_send=TextAnswer(peer, vk)
+        from_id=event.obj.from_id
+        answer_txt=TextAnswer(peer, vk)
         picture = GetPicture(peer,vk)
+        answer_photo=PhotoAnswer(peer,from_id,vk,vk_session)
+        user=UserAnalyze(from_id,vk)
+
         if (event.obj.text == '!пик' or event.obj.text == '!пикча'):
             picture.pic()
         if (event.obj.text == '!гат' or event.obj.text == '!гатари'):
             picture.gat()
         if (event.obj.text.lower().find('!кто') != (-1)):
-            answer_send.answerwho(event.obj.text)
-        if (event.obj.text=="!мать"):
+            answer_txt.answerwho(event.obj.text)
+        if (event.obj.text == "чуш"):
+            answer_photo.chush()
+        if random.randint(1, 200) == 100:
+            answer_photo.chush()
+
+        if (event.obj.text == "[club178122731|*public178122731] top anime"):
+            vk.messages.send(peer_id=event.obj.peer_id, random_id=0, message="завали свой гнилой еблет")
+        if (event.obj.text == "!мать"):
             vk.messages.send(peer_id=event.obj.peer_id, random_id=0, message="мать жива")
-        if (event.obj.text == "*public178122731 top anime"):
-            vk.messages.send(peer_id=event.obj.peer_id, random_id=0, message="завали сой гнилой еблет")
