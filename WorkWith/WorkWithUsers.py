333a13b869866ca10f889a0b7c66a6c3677cc1b5
rendex85 <rendex85@gmail.com>
2021-03-03 09:30:37 +0300

    структура классов более совпадает с общей архитектурой

--- WorkWith/WorkWithUsers.py
+++ WorkWith/WorkWithUsers.py
@@ -1,7 +1,7 @@
 import random
 
 
-class MembersConf():
+class MembersConf:
     def __init__(self, peer_id, vk):
         self.vk = vk
         self.peer_id = peer_id
@@ -17,18 +17,41 @@ class MembersConf():
                 {'full_name': i['name'], 'id': i['id'], 'photo_miniature': i['photo_50'], 'type': 'group',
                  'admin': 'false'})
         for i in self.memb_dict['items']:
-            if ('is_admin' in i) and (i['is_admin'] == True):
+            if ('is_admin' in i) and (i['is_admin']):
                 self.admin_list.append({'id': i['member_id']})
 
-
     def getonemember(self):
-        return (random.choice(self.list_of_members))
+        return random.choice(self.list_of_members)
+
+    @property
+    def admin_list(self):
+        return self.admin_list
+
+    @property
+    def list_of_members(self):
+        return self.list_of_members
+
+    @property
+    def memb_dict(self):
+        return self.memb_dict
+
+    @list_of_members.setter
+    def list_of_members(self, value):
+        self._list_of_members = value
+
+    @memb_dict.setter
+    def memb_dict(self, value):
+        self._memb_dict = value
+
+    @admin_list.setter
+    def admin_list(self, value):
+        self._admin_list = value
 
 
-class UserAnalyze():
+class UserAnalyze:
     def __init__(self, from_id, vk):
         self.vk = vk
         self.from_id = from_id
 
     def getuser(self, params):
-        return (self.vk.users.get(user_ids=self.from_id, fields=params))
+        return self.vk.users.get(user_ids=self.from_id, fields=params)
