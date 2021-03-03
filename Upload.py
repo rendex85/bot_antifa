45f46f058bf2e4806ae9ca65923cb0bc8dbe8953
rendex85 <rendex85@gmail.com>
2021-03-03 09:30:37 +0300

    структура классов более совпадает с общей архитектурой

--- Upload.py
+++ Upload.py
@@ -2,12 +2,12 @@ import requests
 from vk_api import VkUpload
 
 
-
 class PhotoUpload:
     def __init__(self, vk_session):
         self.session = requests.Session()
         self.upload = VkUpload(vk_session)
-    def loadImg (self, image):
+
+    def loadImg(self, image):
         image = self.session.get(image, stream=True)
         photo = self.upload.photo_messages(photos=image.raw)[0]
         return 'photo{}_{}'.format(photo['owner_id'], photo['id'])
--- WorkWith/WorkWithPictures.py
+++ WorkWith/WorkWithPictures.py
@@ -3,6 +3,7 @@ import random
 import vk_api
 
 from Upload import PhotoUpload
+from WorkWith.WorkWithAuth import AuthTools
 from WorkWith.WorkWithPublic import WallWorker
 from WorkWith.WorkWithUsers import UserAnalyze
 from consts import const_array, login_consts
@@ -13,15 +14,11 @@ class GetPicture:
     def __init__(self, peer_id, from_id, vk_session, vk):
         self.peer_id = peer_id
         self.vk = vk
-        self.vk_session1 = vk_api.VkApi(login_consts.phone, login_consts.password)
         self.user = UserAnalyze(from_id, vk)
         self.load = PhotoUpload(vk_session)
 
-    def post(self, url_pic, msg=""):
-        self.vk.messages.send(peer_id=self.peer_id, random_id=0, attachment=url_pic, message=msg)
-
     def pic(self):
-        self.post(self.getImg('-84187544', 'wall', 'photo-84187544'))
+        return self.getImg('-84187544', 'wall', 'photo-84187544')
 
     def gar(self):
         randomnum = random.choice([0, 1])
@@ -29,20 +26,20 @@ class GetPicture:
         photo_url = self.getImg(const_array.account_garik_list[randomnum], choosen_alb,
                                 'photo' + const_array.account_garik_list[randomnum], 1)
         attach = self.load.loadImg(photo_url)
-        self.post(attach)
+        return attach
 
     def gat(self):
         choosen_alb = random.choice(const_array.gatari_alb_lsit)
-        self.post(self.getImg('-7776162', choosen_alb, 'photo-7776162'))
+        return self.getImg('-7776162', choosen_alb, 'photo-7776162')
 
     def chush(self):
         photo = self.user.getuser("photo_50")[0]["photo_50"]
         attach = self.load.loadImg(photo)
-        self.post(attach)
+        return attach
 
     def cat(self):
         attach = self.load.loadImg("https://thiscatdoesnotexist.com/")
-        self.post(attach)
+        return attach
 
     def catWithAnek(self):
         attach = self.load.loadImg("https://thiscatdoesnotexist.com/")
@@ -52,10 +49,11 @@ class GetPicture:
             text = wallWork.getRandomPublicPost()["text"]
         except KeyError:
             text = ""
-        self.post(attach,text)
+        return attach, text
 
     def getImg(self, owner, album, photo_obj_start, type=0):
-        vk1 = self.auth()
+        vk1, longpoll = AuthTools().authByUser()
+
         url_pic = ""
         pic_lenght = (dict(vk1.photos.get(owner_id=owner, album_id=album, offset=1, count='1', photo_sizes=0)))[
             'count']
@@ -67,11 +65,3 @@ class GetPicture:
         elif type == 1:
             url_pic = dict_of_img['items'][0]['sizes'][-1]['url']
         return url_pic
-
-    def auth(self):
-        try:
-            self.vk_session1.auth(token_only=True)
-        except vk_api.AuthError as error_msg:
-            print(error_msg)
-        vk1 = self.vk_session1.get_api()
-        return vk1
