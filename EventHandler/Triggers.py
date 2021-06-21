cc11ebca6f9137ff6fc6fda8c5b8b0e8eac1b67c
rendex85 <rendex85@gmail.com>
2021-06-21 04:00:08 +0300

    all main functions are added

--- EventHandler/Triggers.py
+++
@@ -1,9 +0,0 @@
-from EventHandler.HandlerKernel import BaseHandler
-from WorkWith.WorkWithText import TextAnswer
-
-
-class WhoHandler(BaseHandler):
-    trigger_in = ["!кто", ]
-
-    def preHandler(self):
-        self.message_data.message = TextAnswer(self.obj, self.vk).answer_who()
